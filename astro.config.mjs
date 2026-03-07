9b819774682ee3b37024914522654423b0385989
Gav <info@loosewire.dev>
2026-03-06 22:41:43 -0700

    added cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   site: 'https://loosewire.dev',
   integrations: [sitemap(), mdx(), icon()],
+  adapter: cloudflare(),
 });
